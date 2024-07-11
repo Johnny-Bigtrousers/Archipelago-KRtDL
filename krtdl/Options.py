058e80a8eef45edd8e3c2c98829ffdcfde48ac62
Johnny-Bigtrousers <MysteriousGuyManDudeTheThirdMan@hotmail.com>
2024-07-11 03:39:34 +0100

    Update Options.py

--- krtdl/Options.py
+++ krtdl/Options.py
@@ -302,3 +302,10 @@ data_options: List[str] = [
 
     "death_link",
 ]
+
+def create_option_groups() -> List[OptionGroup]:
+    option_group_list: List[OptionGroup] = []
+    for name, options in krtdl_option_groups.items():
+        option_group_list.append(OptionGroup(name=name, options=options))
+
+    return option_group_list
