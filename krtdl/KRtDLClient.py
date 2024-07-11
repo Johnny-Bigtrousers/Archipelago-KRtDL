f8d1e6c54b908206c42927720340380eefd42b9b
Johnny-Bigtrousers <MysteriousGuyManDudeTheThirdMan@hotmail.com>
2024-07-12 00:35:34 +0100

    Update KRtDLClient.py

--- krtdl/KRtDLClient.py
+++ krtdl/KRtDLClient.py
@@ -264,16 +264,29 @@ class KRtDLContext(CommonContext):
     notification_manager: NotificationManager
     dolphin_bridge: DolphinBridge
     items_handling = 0b111
-    dolphin_sync_task = None
+    dolphin_task = None
     connection_state = ConnectionState.DISCONNECTED
     death_link_enabled = False
 
+    def __init__(self, server_address, password):
+        super().__init__(server_address, password)
+        self.dolphin_bridge = DolphinBridge(logger)
+        self.notification_manager = NotificationManager(HUD_MESSAGE_DURATION, self.dolphin_bridge.send_hud_message)
+    
     async def server_auth(self, password_requested: bool = False):
         if password_requested and not self.password:
             await super(KRtDLContext, self).server_auth(password_requested)
         await self.get_username()
         await self.send_connect()
 
+    def on_package(self, cmd: str, args: dict):
+        if cmd == "Connected":
+            self.slot_data = args["slot_data"]
+            if "death_link" in args["slot_data"]:
+                self.death_link_enabled = bool(args["slot_data"]["death_link"])
+                Utils.async_start(self.update_death_link(
+                    bool(args["slot_data"]["death_link"])))
+    
     def run_gui(self):
         from kvui import GameManager
 
@@ -471,16 +484,16 @@ def launch():
         ctx.run_cli()
 
         logger.info("Running game...")
-        ctx.dolphin_sync_task = asyncio.create_task(dolphin_sync_task(ctx), name="Dolphin Sync")
+        ctx.dolphin_task = asyncio.create_task(dolphin_task(ctx), name="Dolphin Sync")
 
         await ctx.exit_event.wait()
         ctx.server_address = None
 
         await ctx.shutdown()
 
-        if ctx.dolphin_sync_task:
+        if ctx.dolphin_task:
             await asyncio.sleep(3)
-            await ctx.dolphin_sync_task
+            await ctx.dolphin_task
 
     import colorama
 
