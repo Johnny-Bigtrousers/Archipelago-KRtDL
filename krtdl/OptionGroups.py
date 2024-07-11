8e712df143d85a2428b73fc4d482cb3c99c592d2
Johnny-Bigtrousers <MysteriousGuyManDudeTheThirdMan@hotmail.com>
2024-07-11 03:38:14 +0100

    Update OptionGroups.py

--- krtdl/OptionGroups.py
+++ krtdl/OptionGroups.py
@@ -1,1 +1,8 @@
 
+
+def create_option_groups() -> List[OptionGroup]:
+    option_group_list: List[OptionGroup] = []
+    for name, options in krtdl_option_groups.items():
+        option_group_list.append(OptionGroup(name=name, options=options))
+
+    return option_group_list
