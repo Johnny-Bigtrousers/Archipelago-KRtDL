a6837fe8e3aa3e97c8f570a6ff41f7fbf875c3ae
Johnny-Bigtrousers <MysteriousGuyManDudeTheThirdMan@hotmail.com>
2024-07-12 02:02:24 +0100

    Update KRtDLClient.py

--- krtdl/KRtDLClient.py
+++ krtdl/KRtDLClient.py
@@ -202,8 +202,8 @@ class DolphinBridge:
             game_id = ""
             # The first read of the address will be null if the client is faster than the emulator
             self.dolphin_client.read_address(HEADER_ID_ADDRESS, 0)
-            for index in range(6):
-                game_id += self.dolphin_client.read_address(HEADER_ID_ADDRESS, index)
+            game_id = self.dolphin_client.read_address(HEADER_ID_ADDRESS, 0)
+            self.logger.info(game_id)
             self.current_game = None
             if game_id == "SUKE01":
                 self.current_game = version
