dc43c320c703b0cd4c83ed2565c3496e3c68adcd
Johnny-Bigtrousers <MysteriousGuyManDudeTheThirdMan@hotmail.com>
2024-11-19 16:15:10 +0000

    Update KRtDLClient.py

--- krtdl/KRtDLClient.py
+++ krtdl/KRtDLClient.py
@@ -22,7 +22,10 @@ HUD_MESSAGE_ADDRESS = 0x80EB0BE0
 HUD_MESSAGE_DURATION = 10.0
 HUD_MAX_MESSAGE_SIZE = 194
 
-INHALE_ADDRESS = 0x8052a448
+INHALE_ADDRESS = 0x8052a448 # Default value is 40820088, 48000088 for disable
+FLOAT_ADDRESS = 0x80520ab4 # Default value is 4bfcef51, change to 4bfcef51 for dedede to have it but not kirby, 4bfcef2d to disable only dedede's float and 38600000 to disable both
+METAKNIGHT_FLOAT_ADDRESS = 0x805b7d84 # Default value is 41820068, 48000068 for disable
+BANDANADEE_FLOAT_ADDRESS = 0x80519598 # Default value is 41820050, 48000050 for disable
 
 BaseLocationID = 24102011 + 40
 
