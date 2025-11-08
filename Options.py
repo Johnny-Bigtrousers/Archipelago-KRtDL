e2d0b1a18f07cf92f52d7830514dcf5509b34c42
Johnny-Bigtrousers <MysteriousGuyManDudeTheThirdMan@hotmail.com>
2025-11-08 03:03:30 +0000

    Update Options.py
    all cleanup done now I think?

--- Options.py
+++ Options.py
@@ -32,6 +32,85 @@ class EnergySphereHuntRequirement(Range):
     range_end = 240
     default = 80
 
+class ShuffleEnergySpheres(Toggle):
+    """Shuffles all energy spheres into other locations.
+    [Total checks added: 120]"""
+    display_name = "Shuffle Energy Spheres"
+
+class ShufflePartSpheres(Toggle):
+    """Shuffles all Part Spheres into other locations.
+    [May cause BKs related to the Lor Starcutter]
+    [Total checks added: 5]"""
+    display_name = "Shuffle Part Spheres"
+
+class ShuffleEnemies(Choice):
+    """Shuffles the game's enemies around excluding Another Dimension.
+    
+    Light - Enemies will be shuffled with eachother within a given stage's pool.
+    Intense - Enemies will be shuffled with eachother within the entire game's pool.
+    Anomalous - Randomizes enemies out of the game's possible ones. [CREATES HARDER SEEDS]"""
+    display_name = "Shuffle Enemies"
+    default = 0
+    option_off = 0
+    option_light = 1
+    option_intense = 2
+    option_anomalous = 3
+
+class RandomizeEnemyAbilities(Toggle):
+    """Randomizes which enemies give what copy ability."""
+    display_name = "Randomize Enemy Abilities"
+
+class ShuffleBosses(Choice):
+    """Shuffles the game's bosses around excluding Another Dimension.
+    
+    Light - Bosses will be shuffled with eachother.
+    Anomalous - Randomizes boss fights out of the game's possible ones."""
+    display_name = "Shuffle Bosses"
+    default = 0
+    option_off = 0
+    option_light = 1
+    option_anomalous = 2
+
+class StartingWorld(Choice):
+    """Determines the first world of your run. Default is Cookie Country."""
+    display_name = "Starting World"
+    default = 0
+    option_cookie_country = 0
+    option_raisin_ruins = 1
+    option_onion_ocean = 2
+    option_white_wafers = 3
+    option_nutty_noon = 4
+    option_egg_engines = 5
+    option_dangerous_dinner = 6
+
+class StartWithAllWorlds(Toggle):
+    """Unlocks all of the worlds and their Stage 1s from the start.
+    [Some worlds may be locked out by Lor Starcutter access]"""
+    display_name = "Start with all Worlds"
+
+class StartWithAllStages(Toggle):
+    """Unlocks all of the non-boss stages from the start. Beating all of a level's stages unlocks the boss stage slot."""
+    display_name = "Start with all Stages"
+
+
+
+
+class ShuffleStages(Choice):
+    """Shuffles the game's stages around.
+    
+    Light - Shuffles stages within a given world.
+    Intense - Shuffles all of the game's stages with eachother."""
+    display_name = "Shuffle Stages"
+    default = 0
+    option_off = 0
+    option_light = 1
+    option_intense = 2
+
+class ShuffleBossStages(Toggle):
+    """Shuffles the game's boss stages around.
+    [If Shuffle Stages is turned on to Intense then boss stages can possibly end up in other worlds]"""
+    display_name = "Shuffle Boss Stages"
+
 class ShuffleCookieCountry(DefaultOnToggle):
     """If enabled, allows Cookie Country to be shuffled with other worlds."""
     display_name = "Shuffle Cookie Country"
@@ -60,97 +139,27 @@ class ShuffleDangerousDinner(DefaultOnToggle):
     """If enabled, allows Dangerous Dinner to be shuffled with other worlds."""
     display_name = "Shuffle Dangerous Dinner"
 
-class StartingWorld(Choice):
-    """Determines the first world of your run. Default is Cookie Country."""
-    display_name = "Starting World"
-    default = 0
-    option_cookie_country = 0
-    option_raisin_ruins = 1
-    option_onion_ocean = 2
-    option_white_wafers = 3
-    option_nutty_noon = 4
-    option_egg_engines = 5
-    option_dangerous_dinner = 6
 
-class UnlockWorlds(Toggle):
-    """Unlocks all of the worlds from the start.
-    [Some worlds may be locked out by Lor Starcutter access]"""
-    display_name = "Unlock Worlds"
 
-class ShuffleStages(Choice):
-    """Shuffles the game's stages around.
-    
-    Light - Shuffles stages within a given world.
-    Intense - Shuffles all of the game's stages with eachother."""
-    display_name = "Shuffle Stages"
-    default = 0
-    option_off = 0
-    option_light = 1
-    option_intense = 2
-
-class StartWithAllStages(Toggle):
-    """Unlocks all of the non-boss stages from the start. Beating all of a level's stages unlocks the boss stage slot."""
-    display_name = "Unlock Worlds"
-
-class ShuffleBossStages(Toggle):
-    """Shuffles the game's boss stages around.
-    [If Shuffle Stages is turned on to Intense then boss stages can possibly end up in other worlds]"""
-    display_name = "Shuffle Boss Stages"
-
-class ShuffleBosses(Choice):
-    """Shuffles the game's bosses around excluding Another Dimension.
-    
-    Light - Bosses will be shuffled with eachother.
-    Anomalous - Randomizes boss fights out of the game's possible ones."""
-    display_name = "Shuffle Bosses"
-    default = 0
-    option_off = 0
-    option_light = 1
-    option_anomalous = 2
-
-class ShuffleEnemies(Choice):
-    """Shuffles the game's enemies around excluding Another Dimension.
-    
-    Light - Enemies will be shuffled with eachother within a given stage's pool.
-    Intense - Enemies will be shuffled with eachother within the entire game's pool.
-    Anomalous - Randomizes enemies out of the game's possible ones. [CREATES HARDER SEEDS]"""
-    display_name = "Shuffle Enemies"
-    default = 0
-    option_off = 0
-    option_light = 1
-    option_intense = 2
-    option_anomalous = 3
 
 class ShuffleCopyAbilities(Toggle):
-    """Shuffles what enemies give what copy abilities."""
+    """Requires individual Copy Abilities to be unlocked to be able to obtain them."""
     display_name = "Shuffle Copy Abilities"
 
-class RandomizeCopyAbilities(Toggle):
-    """Requires individual Copy Abilities to be unlocked to be able to obtain them."""
-    display_name = "Randomize Copy Abilities"
-
-class RandomizeLandia(Toggle):
+class ShuffleLandia(Toggle):
     """Requires you to find Landia in order to enter Another Dimension."""
-    display_name = "Randomize Landia"
+    display_name = "Shuffle Landia"
 
-class RandomizeMoves(Toggle):
+class ShuffleMoves(Toggle):
     """If enabled, requires some of Kirby's basic moves to be unlocked to be able to use them."""
-    display_name = "Randomize Moves"
+    display_name = "Shuffle Moves"
 
-class RandomizeItems(Toggle):
-    """Requires items such as Invincibility Candy or the Crackler to be unlocked to be able to find them in levels."""
-    display_name = "Randomize Items"
+class ShuffleLevelItems(Toggle):
+    """Requires items such as Invincibility Candy or Crackler to be unlocked to be able to find them in levels."""
+    display_name = "Shuffle Items"
 
-class EnergySphereSanity(Toggle):
-    """Turns all Energy Spheres into checks.
-    [Total checks added: 120]"""
-    display_name = "Energy Spheresanity"
 
-class PartSphereSanity(Toggle):
-    """Turns all Part Spheres into checks.
-    [May cause BKs related to the Lor Starcutter]
-    [Total checks added: 5]"""
-    display_name = "Part Spheresanity"
+
 
 class StarSanity(Toggle):
     """Turns all guaranteed Gold Stars (ones not gained from Flowers) into checks.
@@ -189,15 +198,15 @@ class TrapChance(Range):
     display_name = "Trap Chance"
     range_start = 0
     range_end = 100
-    default = 0
+    default = 5
 
 class SleepWeight(Range):
     """The weight of Sleep traps in the trap pool.
-    Forces all players to use Sleep if possible."""
+    Forces all players to use the Sleep ability if possible."""
     display_name = "Sleep Weight"
     range_start = 0
     range_end = 100
-    default = 40
+    default = 50
 
 class EjectWeight(Range):
     """The weight of Eject traps in the trap pool.
@@ -205,7 +214,7 @@ class EjectWeight(Range):
     display_name = "Eject Weight"
     range_start = 0
     range_end = 100
-    default = 20
+    default = 10
 
 class MouthfulWeight(Range):
     """The weight of Mouthful traps in the trap pool.
@@ -216,27 +225,32 @@ class MouthfulWeight(Range):
     default = 40
 
 
+
+
 @dataclass
 class KRtDLOptions(PerGameCommonOptions):
     """Options for Kirby's Return to Dream Land."""
     
-    goal:                           Goal
+    goal: Goal
     energy_sphere_hunt_requirement: EnergySphereHuntRequirement
-    ShuffleEnemies:            ShuffleEnemies
+    shuffle_energy_spheres: ShuffleEnergySpheres
+    shuffle_part_spheres: ShufflePartSpheres
+    shuffle_enemies: ShuffleEnemies
     randomize_enemy_abilities: RandomizeEnemyAbilities
-    ShuffleBosses:             ShuffleBosses
-    starting_world:                 StartingWorld
-    start_with_all_stages:          StartWithAllStages
+    shuffle_bosses: ShuffleBosses
+    starting_world: StartingWorld
+    start_with_all_worlds: StartWithAllWorlds
+    start_with_all_stages: StartWithAllStages
 
-    ShuffleStages:                  ShuffleStages
-    ShuffleBossStages:              ShuffleBossStages
-    shuffle_cookie_country:         ShuffleCookieCountry
-    shuffle_raisin_ruins:           ShuffleRaisinRuins
-    shuffle_onion_ocean:            ShuffleOnionOcean
-    shuffle_white_wafers:           ShuffleWhiteWafers
-    shuffle_nutty_noon:             ShuffleNuttyNoon
-    shuffle_egg_engines:            ShuffleEggEngines
-    shuffle_dangerous_dinner:       ShuffleDangerousDinner
+    shuffle_stages: ShuffleStages
+    shuffle_boss_stages: ShuffleBossStages
+    shuffle_cookie_country: ShuffleCookieCountry
+    shuffle_raisin_ruins: ShuffleRaisinRuins
+    shuffle_onion_ocean: ShuffleOnionOcean
+    shuffle_white_wafers: ShuffleWhiteWafers
+    shuffle_nutty_noon: ShuffleNuttyNoon
+    shuffle_egg_engines: ShuffleEggEngines
+    shuffle_dangerous_dinner: ShuffleDangerousDinner
     
     shuffle_copy_abilities: ShuffleCopyAbilities
     shuffle_landia: ShuffleLandia
@@ -249,6 +263,7 @@ class KRtDLOptions(PerGameCommonOptions):
     food_sanity: FoodSanity
     one_up_sanity: OneUpSanity
     maxim_sanity: MaximSanity
+    extra_sanity: ExtraSanity
 
     trap_chance: TrapChance
     sleep_weight: SleepWeight
@@ -262,32 +277,35 @@ krtdl_option_groups = [
         "Basic Options", 
         [Goal, 
          EnergySphereHuntRequirement, 
-         ShuffleCookieCountry, 
-         ShuffleRaisinRuins, 
-         ShuffleOnionOcean, 
-         ShuffleWhiteWafers, 
-         ShuffleNuttyNoon, 
-         ShuffleEggEngines, 
-         ShuffleDangerousDinner, 
+         ShuffleEnergySpheres,
+         ShufflePartSpheres,
+         ShuffleEnemies,
+         RandomizeEnemyAbilities,
+         ShuffleBosses,
          StartingWorld, 
-         UnlockWorlds],
+         StartWithAllWorlds,
+         StartWithAllStages],
     ),
 
     OptionGroup(
         "Stage Options",
         [ShuffleStages, 
          ShuffleBossStages, 
-         ShuffleBosses, 
-         ShuffleEnemies],
+         ShuffleCookieCountry, 
+         ShuffleRaisinRuins, 
+         ShuffleOnionOcean, 
+         ShuffleWhiteWafers, 
+         ShuffleNuttyNoon, 
+         ShuffleEggEngines, 
+         ShuffleDangerousDinner],
     ),
 
     OptionGroup(
         "Item Options", 
         [ShuffleCopyAbilities, 
-         RandomizeCopyAbilities, 
-         RandomizeLandia, 
-         RandomizeMoves, 
-         RandomizeItems],
+         ShuffleLandia, 
+         ShuffleMoves, 
+         ShuffleLevelItems],
     ),
 
     OptionGroup(
@@ -297,7 +315,8 @@ krtdl_option_groups = [
          BlueStarSanity, 
          FoodSanity, 
          OneUpSanity, 
-         MaximSanity],
+         MaximSanity,
+         ExtraSanity],
     ),
 
     OptionGroup(
